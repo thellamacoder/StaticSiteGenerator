5d45f127c8a09595bc97bc4c2a894598f2a5b7ac
thellamacoder <scott.s.klein@gmail.com>
2025-12-16 19:20:33 -0500

    finished the test_htmlnode.py file

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -7,10 +7,21 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("<a>", "Hello World!", None, {"href": "https://www.google.com", "target": "_blank"},)
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
 
+    def test_repr(self):
+        node = HTMLNode("a", "Hello World!", None, {"href": "https://www.google.com"},)
+        self.assertEqual(node.__repr__(), "HTMLNode(Tag: a, Value: Hello World!, Children: None, Props: {'href': 'https://www.google.com'})")
 
 
+    def test_values(self):
+         node = HTMLNode("div", "I'm happy!")
 
+         self.assertEqual(node.tag, "div")
 
+         self.assertEqual(node.value, "I'm happy!")
+         
+         self.assertEqual(node.children, None)
+         
+         self.assertEqual(node.props, None)
 
 
 
