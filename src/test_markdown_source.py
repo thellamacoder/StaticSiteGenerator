36ab09095465ab4fc515eab6795b801065516e21
thellamacoder <scott.s.klein@gmail.com>
2025-12-29 08:17:01 -0500

    added tests for the markdown function

--- src/test_markdown_source.py
+++ src/test_markdown_source.py
@@ -4,13 +4,27 @@ from textnode import TextNode, TextType
 from markdown_source import split_nodes_delimiter
 
 class TestMarkdownToTextNodeList(unittest.TestCase):
-	def test_inline_markdown(self):
+	
+	def test_inline_markdown_code(self):
 		node = TextNode("This is text with a `code block` word", TextType.TEXT)
 		new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 		self.assertEqual(new_nodes, 
 			[
-			TextNode("This is text with a ", 
-			TextType.TEXT), 
+			TextNode("This is text with a ", TextType.TEXT), 
 			TextNode("code block", TextType.CODE), 
 			TextNode(" word", TextType.TEXT)
 			])
+
+	def test_inline_markdown_bold(self):
+		node = TextNode("This is *bold* text", TextType.TEXT)
+		new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
+		self.assertEqual(new_nodes, 
+			[
+			TextNode("This is ", TextType.TEXT),
+			TextNode("bold", TextType.BOLD),
+			TextNode(" text", TextType.TEXT)
+        ])
+	def test_inline_markdown_italic(self):
+		node = TextNode("This is _italic_ text", TextType.TEXT)
+		
+		
