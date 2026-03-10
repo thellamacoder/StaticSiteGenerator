b9f957145ebcbe0f1e16e393f64e94beabaf1e84
thellamacoder <scott.s.klein@gmail.com>
2026-03-10 17:08:21 -0400

    began CH5 L1

--- src/main.py
+++ src/main.py
@@ -1,22 +1,19 @@
 from textnode import TextNode, TextType
 from markdown_source import *
 from markdown_blocks import *
+import os
 
 def main():
-	
-	print("----")
-	md = """
-# This is a heading
-
-This is another paragraph with _italic_ text and `code` here
-This is the same paragraph on a new line
-	
--This is a list
--with items"""
-	print(markdown_to_blocks(md))
-	print("----")
+ setup_site()
 
 
 
+
+def setup_site():
+    # clear public
+
+    # find contents of source
+
+    # copy contents of source into public
 
 main()
