08a59365192afa93039c2075fcc286a481fb6a89
thellamacoder <scott.s.klein@gmail.com>
2026-03-11 08:02:52 -0400

    worked on detecting if the public folder exitst and replacing it

--- src/main.py
+++ src/main.py
@@ -2,18 +2,31 @@ from textnode import TextNode, TextType
 from markdown_source import *
 from markdown_blocks import *
 import os
+import shutil
 
 def main():
- setup_site()
-
+ 
+    pass
 
 
 
 def setup_site():
+    
+    public = os.path.exists("public")
+    if public:
+       print("the public file exists")
+       shutil.rmtree("public/")
+       os.mkdir("public")
+    else:
+        print("that folder does not exist")
+        print(os.listdir())
     # clear public
 
     # find contents of source
 
     # copy contents of source into public
 
+    
+
 main()
+setup_site()
