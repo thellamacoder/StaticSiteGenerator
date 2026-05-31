9a37fe9a6b6ac51b18872fbe734439bfa36557c8
thellamacoder <scott.s.klein@gmail.com>
2026-05-31 18:48:48 -0400

    CH5 L5: Deploying to the Internet

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
-
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
-def main():
+
+def main()->None:
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
        shutil.rmtree(dir_path_public)
@@ -24,6 +30,7 @@ def main():
         dir_path_content,
         template_path,
         dir_path_public,
+        basepath
     )
 
 main()
